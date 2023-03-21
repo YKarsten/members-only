eebaa175c3a23a1fb0bc6c529b956436df35c21b
YKarsten <yannik.karsten@web.de>
2023-03-21 16:00:01 +0100

    Set #new & #create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
   # before_action :authenticate_user!
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # Enable for normal top level User attributes
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    # Enable for hashed User attributes
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
