867e027f8f05b79a7c0daf1dfcac0d6f8274a1bc
YKarsten <yannik.karsten@web.de>
2023-04-17 14:27:19 +0200

    Redirect to index & remove author_id from strong parameters as the form doesnt submit it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     if @post.save
       flash[:success] = 'Great! Your post has been created!'
-      redirect_to new_post_path # go to show page for @post
+      redirect_to action: 'index' # go to show page for @post
     else
       flash.now[:error] = 'Rats! Fix your mistakes, please.'
       render :new, status: :unprocessable_entity
@@ -24,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def allowed_post_params
-    params.require(:post).permit(:title, :body, :author_id)
+    params.require(:post).permit(:title, :body)
   end
 end
