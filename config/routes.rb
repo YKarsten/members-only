ec6d32f1200c497fbff447fdc38579f047bcaddb
YKarsten <yannik.karsten@web.de>
2023-04-17 14:31:19 +0200

    change root path to show posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "posts#index"
   # root to: 'home#index'
   resources :users, only: [:new, :create]
   resources :posts, only: [:new, :create, :index]
